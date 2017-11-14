4ecb1180af5c6f5500436b493e1ffba0f1bbef59
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-14 12:54:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solutions in this file
+const arr = [];
+
 function printBadges(arrayNames){
   for (let i = 0; i < arrayNames.length; i++) {
   console.log('Welcome ' + arrayNames[i] + '! You are employee #' + (i+1) + '.');

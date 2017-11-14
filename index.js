6fde692e4edbb4c024015b02beda0adf2643c8ae
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-14 13:05:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Code your solutions in this file
 
-function printBadges(names){
+function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
   console.log('Welcome ' + names[i] + '! You are employee #' + (i+1) + '.');
 
 }
 return names;
+}
+
+
+function tailsNeverFails () {
+  while ()
 }
